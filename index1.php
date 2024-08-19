72a1c8979f6d1c402d4614cbb35a53ec84e408f2
tuanbui04 <buituan2611@gmail.com>
2024-08-19 15:40:28 +0700

    init baitap

---
+++ index1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo '<p style="text-align: center; color:green;"> Xin chào các bạn </p>';
+echo '<button style="background-color:red;"> Click </button>';
+echo '<a target="_blank" href="https://www.youtube.com"> youtube </a>'
+?>
+</body>
+</html>
